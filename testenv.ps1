fe65fb4dad4bff811165afb891b5ec1a03b12e5a
Christopher A. Mosher <cmosher01@gmail.com>
2022-11-17 21:44:43 -0500

    appdata test

--- testenv.ps1
+++ testenv.ps1
@@ -1,1 +1,5 @@
+$env:APPDATA
 [System.Environment]::SetEnvironmentVariable("APPDATA", "D:\a\_temp\foo", [System.EnvironmentVariableTarget]::Machine)
+$env:APPDATA
+[System.Environment]::SetEnvironmentVariable("APPDATA", "D:\a\_temp\foo", [System.EnvironmentVariableTarget]::User)
+$env:APPDATA
